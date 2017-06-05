e053b34fa778f96ee8da2c2313394df7d9467ce8
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-05 23:11:14 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -3,99 +3,72 @@ var d3 = require("d3");
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("data.sqlite");
 
-//db.run("DELETE FROM data");
-
+db.run("DELETE FROM data");
 var p = 0;
-
 var formatTime = d3.timeFormat("%Y-%m-%d");
-
 var myDate = new Date();
 var dayOfMonth = myDate.getDate();
-myDate.setDate(dayOfMonth - 2);
-
+myDate.setDate(dayOfMonth - 3);
 var start  = formatTime(myDate);
 console.log(start);
-
-
 var end  = formatTime(new Date());
 console.log(end);
-
-
-   
+ 
 function piv(){  
 p++;
 client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts?offset='+start})
 		.then(function (data) {
-						 
-		
-			var dataset = data.getJSON().data;
-			
-			start = data.getJSON().next_page.offset;			
-						
-			console.log(start)
+			var dataset = data.getJSON().data;		
+			start = data.getJSON().next_page.offset;								
+			console.log(start);
 			
 			return dataset;
 		})	
-		.then(function (dataset) {	
-		
+		.then(function (dataset) {			
 			dataset.forEach(function(item) {
 				client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts/'+item.id})
-					.then(function (data) {
-				
-		
-
+					.then(function (data) {	
+//////////SQLite//////////////
 
 var change = data.getJSON().data.changes[data.getJSON().data.changes.length-1].rationaleTypes[0];
-//console.log(change)			
-
 if(change=="itemPriceVariation"){
-	
+	var change0 = data.getJSON().data.changes[0].date;
+	var dateSigned = data.getJSON().data.dateSigned;
+	var amount = data.getJSON().data.value.amount;
 	var tender_id = data.getJSON().data.tender_id;
 	
 	client.request({url: 'https://public.api.openprocurement.org/api/2.3/tenders/'+tender_id})
 					.then(function (data) {
-						
-db.serialize(function() {
+							
+	db.serialize(function() {	
+	db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,dateSigned TEXT,change0 TEXT,tenderID TEXT,procuringEntity TEXT,numberOfBids INT,amount INT,cpv TEXT)");
+	var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?,?)");
+  	
+	statement.run(item.dateModified,dateSigned,change0,data.getJSON().data.tenderID,data.getJSON().data.procuringEntity.name,data.getJSON().data.numberOfBids,amount,data.getJSON().data.items[0].classification.description);
+	//console.log(change);
+	statement.finalize();
+	});
 	
-  db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,tenderID TEXT,procuringEntity TEXT,suppliers TEXT,numberOfBids INT,amount INT,cpv TEXT)");
-
-  // Insert a new record
-  var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?)");
-  
-  statement.run(item.dateModified,data.getJSON().data.tenderID,data.getJSON().data.procuringEntity.name,data.getJSON().data.suppliers.name,data.getJSON().data.numberOfBids,data.getJSON().data.value.amount,data.getJSON().data.items[0].classification.description);
-
-  statement.finalize();
-});
-db.close();		
-})
-						
-
+	})		
 }
-	
+//////////SQLite//////////////	
 					})
 					.catch(function  (error) {
-						//console.log("error_detale")
-						
+						//console.log("error_detale")				
 					});  
-				});
-		
+			});	
 		})
 		.then(function () {	
-		if (start.replace(/T.*/, "") != end) {piv ();}	
-		else {
-			console.log("STOP")
-			console.log(start.replace(/T.*/, ""))
-	
-		}		
-							
+			if (start.replace(/T.*/, "") != end) {piv ();}	
+			else {
+				console.log("STOP")
+				//console.log(start.replace(/T.*/, ""))
+			}							
 		})
 		.catch( function (error) {
 			console.log("error")
 			piv ();
-		});   
-		
-					
-
+		});   					
 }
 
 piv ();	
