7b3818f310c480e302c8a54ff42f49d986c52806
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-05 07:37:38 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,59 +1,95 @@
-// This is a template for a Node.js scraper on morph.io (https://morph.io)
-
-var cheerio = require("cheerio");
-var request = require("request");
+var client = require('http-api-client');
 var sqlite3 = require("sqlite3").verbose();
 
-function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
-	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
-	});
+// Open a database handle
+var db = new sqlite3.Database("data.sqlite");
+
+
+	
+var currentCount =  "2017-05-29T10:53:37.484602+03:00"
+var p=0; var p2=0;
+   
+   
+function piv(){  
+p++;
+client.request({url: 'https://public.api.openprocurement.org/api/2.3/plans?offset='+currentCount})
+		.then(function (data) {
+						 
+		
+			var dataset = data.getJSON().data;
+			
+			currentCount = data.getJSON().next_page.offset;			
+			console.log(currentCount)
+			
+			return dataset;
+		})	
+		.then(function (dataset) {	
+		
+			dataset.forEach(function(item) {
+				client.request({url: 'https://public.api.openprocurement.org/api/0/plans/'+item.id})
+					.then(function (data) {
+/*
+var res = '{
+"id":"'+data.getJSON().data.id+'",
+"datePublished":"'+data.getJSON().data.datePublished+'",
+"cpv":"'+data.getJSON().data.classification.id+'",
+"name":"'+data.getJSON().data.procuringEntity.identifier.id+'", 
+"amount":'+data.getJSON().data.budget.amount+', 
+"currency":"'+data.getJSON().data.budget.currency+'", 
+"procurementMethod":"'+data.getJSON().data.tender.procurementMethod+'",
+"procurementMethodType":"'+data.getJSON().data.tender.procurementMethodType+'",
+"startDate":"'+data.getJSON().data.tender.tenderPeriod.startDate+'"},'
+*/					
+				
+					
+db.serialize(function() {
+
+  // Create new table
+  db.run("CREATE TABLE IF NOT EXISTS data (id TEXT,datePublished TEXT,cpv TEXT,amount INT)");
+
+  
+  // Insert a new record
+  var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?)");
+
+statement.run(data.getJSON().data.id,data.getJSON().data.datePublished,data.getJSON().data.classification.id,data.getJSON().data.budget.amount);
+  //else none;
+   //console.log(item.dateModified)
+  statement.finalize();
+});
+
+					})
+					.catch(function  (error) {
+						console.log("error_detale")
+						
+					});  
+				});
+		
+		})
+		.then(function () {	
+		if (p<10){piv ();}		
+		else {
+			console.log("stop")
+				p=0;
+				p2++;
+				console.log(p2)
+			setTimeout(function() {
+			
+				if (p2 < 2) {
+					piv ();
+				}
+				else {console.log("STOP")}
+				}, 5000);
+		}		
+							
+		})
+		.catch( function (error) {
+		console.log("error")
+		piv ();
+		});   
+		
+		
+			
+
 }
 
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
-}
-
-function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
-}
-
-function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
-		}
-
-		callback(body);
-	});
-}
-
-function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
-
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
-
-		readRows(db);
-
-		db.close();
-	});
-}
-
-initDatabase(run);
+piv ();	
