754f73446cd0ce2ac0a3f0bd79ccf58da8b14673
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-11 12:46:13 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -4,7 +4,7 @@ var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("data.sqlite");
 
 client.request({
-    url: 'https://api.morph.io/Mraleksa/Plans1/data.json?key=kySf3vNeyObyXvbJKG4u&query=select%20*%20from%20%27data%27%20limit%2010'
+    url: 'https://api.morph.io/Mraleksa/Plans1/data.json?key=kySf3vNeyObyXvbJKG4u&query=select%20*%20from%20%27data%27%20limit%2020'
 }).then(function (response) {
     
 	console.log(response.getJSON());
